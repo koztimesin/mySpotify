f804f70f7f65a2063bfb047facb1556fc5904d40
koztimesin <a.isumairu01@gmail.com>
2022-12-11 19:32:04 +0300

    request to get current user profile

--- mySpotify/Managers/APICaller.swift
+++ mySpotify/Managers/APICaller.swift
@@ -7,12 +7,50 @@
 
 import Foundation
 
+private enum Constants {
+    static let baseAPIURL = "https://api.spotify.com/v1"
+}
+
 final class APICaller {
     static let shared = APICaller()
     
     private init() {}
     
+    enum APIError: Error {
+        case failedToGetData
+    }
+    
     public func getCurrentUserProfile(completion: @escaping(Result<UserProfile, Error>) -> Void) {
-        
+        createRequest(with: URL(string: Constants.baseAPIURL + "/me"), type: .GET) { baseRequest in
+            let task = URLSession.shared.dataTask(with: baseRequest) { data, _, error in
+                guard let data = data, error == nil else {
+                    completion(.failure(APIError.failedToGetData))
+                    return
+                }
+                
+                do {
+                    let result = try JSONSerialization.jsonObject(with: data)
+                    print(result)
+                } catch {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    enum HTTPMethod: String {
+        case GET
+        case POST
+    }
+    
+    private func createRequest(with url: URL?, type: HTTPMethod, completion: @escaping (URLRequest) -> Void) {
+        AuthManager.shared.withValidToken { token in
+            guard let url = url else { return }
+            var request = URLRequest(url: url)
+            request.setValue("Bearer \(token)", forHTTPHeaderField: "Authorization")
+            request.httpMethod = type.rawValue
+            request.timeoutInterval = 30
+            completion(request)
+        }
     }
 }
