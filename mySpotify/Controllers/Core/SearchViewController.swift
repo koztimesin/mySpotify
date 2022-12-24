15089bd4d9e6b6fcc8e85d6bc4606618d6eb72e1
koztimesin <a.isumairu01@gmail.com>
2022-12-24 20:20:29 +0300

    added search bar

--- mySpotify/Controllers/Core/SearchViewController.swift
+++ mySpotify/Controllers/Core/SearchViewController.swift
@@ -8,11 +8,38 @@
 import UIKit
 
 class SearchViewController: UIViewController {
-
+    
+    private lazy var searchController: UISearchController = {
+        let results = UIViewController()
+        results.view.backgroundColor = .systemBlue
+        let vc = UISearchController(searchResultsController: results)
+        vc.searchBar.placeholder = "Songs, Artists, Albums"
+        vc.searchBar.searchBarStyle = .minimal
+        vc.definesPresentationContext = true
+        
+        return vc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
     }
 
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text,
+        !query.trimmingCharacters(in: .whitespaces).isEmpty else {
+            return
+        }
+        
+        // Perform Search
+        
+    }
+    
 }
