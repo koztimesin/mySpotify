126dbeced4c80ec6c2ceb5fffa363f88710ec203
koztimesin <a.isumairu01@gmail.com>
2023-01-13 16:39:05 +0300

    created two vc

---
+++ mySpotify/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryAlbumsViewController.swift
+//  mySpotify
+//
+//  Created by koztimesin on 13.01.2023.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ mySpotify/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  mySpotify
+//
+//  Created by koztimesin on 13.01.2023.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+
+}
