29beb71a991cbab0fad966e011a2b2cb14cb7175
koztimesin <a.isumairu01@gmail.com>
2023-01-16 01:25:04 +0300

    constructed welcome vc

--- mySpotify/Controllers/Other/WelcomeViewController.swift
+++ mySpotify/Controllers/Other/WelcomeViewController.swift
@@ -14,26 +14,94 @@ class WelcomeViewController: UIViewController {
         let button = UIButton()
         button.backgroundColor = .white
         button.setTitle("Sign In with Spotify", for: .normal)
-        button.setTitleColor(.systemBlue, for: .normal)
+        button.setTitleColor(.black, for: .normal)
         button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        
         
         return button
     }()
-
+    
+    private lazy var imageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.image = UIImage(named: "welcome")
+        
+        return image
+    }()
+    
+    private lazy var overlayView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.7
+        
+        return view
+    }()
+    
+    private lazy var logoImageView: UIImageView = {
+        let image = UIImageView(image: UIImage(named: "pngLogo"))
+        image.contentMode = .scaleAspectFit
+        
+        return image
+    }()
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Listen to Millions\nof Songs on\nthe go."
+        label.textAlignment = .center
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.font = .systemFont(ofSize: 32, weight: .semibold)
+        
+        return label
+    }()
+    
+    private lazy var vStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        return stack
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         title = "mySpotify"
-        view.backgroundColor = .systemGreen
+        navigationController?.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        view.addSubview(imageView)
+        view.addSubview(overlayView)
         view.addSubview(signInButton)
-        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        view.addSubview(vStack)
+        vStack.addArrangedSubview(logoImageView)
+        vStack.addArrangedSubview(label)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+        imageView.frame = view.bounds
+        
+        overlayView.frame = view.bounds
+        
         signInButton.snp.makeConstraints { make in
-            make.centerY.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaInsets)
             make.leading.trailing.equalToSuperview().inset(100)
+        }
+        
+        vStack.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(40)
+        }
+        
+        logoImageView.snp.makeConstraints { make in
+            make.size.equalTo(120)
+            make.centerX.equalToSuperview()
+        }
+        
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
         }
     }
     
